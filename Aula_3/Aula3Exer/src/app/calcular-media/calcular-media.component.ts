e1a48fe9e21a62dd2c198d7777ab27a85b1be8bc
João Souza <joaodealmedassouza@gmail.com>
2024-09-24 19:08:33 -0300

    aula3

--- Aula_3/Aula3Exer/src/app/calcular-media/calcular-media.component.ts
+++ Aula_3/Aula3Exer/src/app/calcular-media/calcular-media.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './calcular-media.component.html',
   styleUrl: './calcular-media.component.css'
 })
-export class CalcularMediaComponent {
 
+export class CalcularMediaComponent {
+  ac1: number = 0;
+  ac2: number = 0;
+  af: number = 0;
+  ag: number = 0;
+  media: number = 0;
+
+  constructor() {}
+
+  calcularMedia(): void {
+    this.media = (this.ac1 + this.ac2 + this.af + this.ag) / 4;
+  }
 }
