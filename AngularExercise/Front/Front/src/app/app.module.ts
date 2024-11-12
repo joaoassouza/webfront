42147ef54554d986858ec755409d87a1632ee5c2
João Souza <joaodealmedassouza@gmail.com>
2024-11-11 23:49:59 -0300

    Aluno Service exercise

--- AngularExercise/Front/Front/src/app/app.module.ts
+++ AngularExercise/Front/Front/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunoComponent } from './components/aluno/aluno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
